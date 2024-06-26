cb1c778441048bc70904fc9eec37668a9e5556be
Lee-Dongheon-kaist <dongheon6@kaist.ac.kr>
2024-06-26 17:08:24 +0900

    .

--- sample_html/html/search/functions_0.js
+++ sample_html/html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['accel_11',['Accel',['../classCar.html#a4d7e0b5cae548739a9ecfcd55a5bf35e',1,'Car']]]
+  ['accel_47',['Accel',['../classCar.html#a4d7e0b5cae548739a9ecfcd55a5bf35e',1,'Car']]]
 ];
